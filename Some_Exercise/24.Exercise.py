fcc2b4fe8143b3caddf6c6657443d2166e713cad
Latif1218 <mdsabdullotif@gmail.com>
2025-12-10 15:39:44 +0600

    add

--- Some_Exercise/24.Exercise.py
+++ Some_Exercise/24.Exercise.py
@@ -44,25 +44,24 @@
 
 
 
-# def count_substring(string, sub_string):
-#     return
+def count_substring(string, sub_string):
+    count = 0
+    for i in range((len(string)) - ((len(sub_string))-1)):
+        if (string[i:i + len(sub_string)] == sub_string):
+            count += 1
+    return count
 
-# if __name__ == '__main__':
-#     string = input().strip()
-#     sub_string = input().strip()
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
     
-#     count = count_substring(string, sub_string)
-#     print(count)
+    count = count_substring(string, sub_string)
+    print(count)
 
 
 
-string = "ABCDCDC"
-sub_string = "CDC"
 
-count = 0
-for i in range(0, len(string)):
-    if sub_string == i:
-        count += 0
-print(count)
+
+
     
     
